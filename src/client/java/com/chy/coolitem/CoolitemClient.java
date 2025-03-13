9e163b74ed519e939939d5a0cc5b07751ef02602
codessh <124234780+codessh@users.noreply.github.com>
2025-03-13 19:43:57 +0800

    Update CoolitemClient.java

--- src/client/java/com/chy/coolitem/CoolitemClient.java
+++ src/client/java/com/chy/coolitem/CoolitemClient.java
@@ -1,6 +1,7 @@
 package com.chy.coolitem;
 
 import com.chy.coolitem.gui.CoolitemGuiHandlerType;
+import com.chy.coolitem.gui.CopierGui;
 import com.chy.coolitem.gui.DiamondCraftingTableGui;
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
@@ -9,5 +10,6 @@ public class CoolitemClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		HandledScreens.register(CoolitemGuiHandlerType.DIAMOND_CRAFTING_TABLE_GUI_HANDLER, DiamondCraftingTableGui::new);
+		HandledScreens.register(CoolitemGuiHandlerType.COPIER_GUI_HANDLER, CopierGui::new);
 	}
 }
