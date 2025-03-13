994d4cca11344ba9bfc82ff6de1836330de3ae92
codessh <124234780+codessh@users.noreply.github.com>
2025-03-13 19:48:12 +0800

    Add files via upload

---
+++ src/client/java/com/chy/coolitem/gui/CopierGui.java
@@ -0,0 +1,33 @@
+package com.chy.coolitem.gui;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class CopierGui extends HandledScreen<CopierGuiHandler> {
+    private static final Identifier Gui = Identifier.of("coolitem","textures/gui/copier.png");
+    public CopierGui(CopierGuiHandler handler, PlayerInventory inventory, Text title) {
+        super(handler, inventory, title);
+        this.backgroundHeight=256;
+        this.backgroundWidth=256;
+        this.playerInventoryTitleX=-9999;
+        this.playerInventoryTitleY=-9999;
+    }
+
+    @Override
+    protected void drawBackground(DrawContext context, float delta, int mouseX, int mouseY) {
+        int i = (this.width - this.backgroundWidth) / 2;
+        int j = (this.height - this.backgroundHeight) / 2;
+        context.drawTexture(Gui, i, j, 0, 0, this.backgroundWidth, this.backgroundHeight);
+    }
+
+    @Override
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        super.render(context, mouseX, mouseY, delta);
+        this.drawMouseoverTooltip(context, mouseX, mouseY);
+    }
+
+}
